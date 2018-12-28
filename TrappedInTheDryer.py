ee3357eda4b3c97e77abf6530ba992ddad5635fc
Julia <jbian@ucvts.org>
2018-12-28 18:14:37 +0000

    character class

--- TrappedInTheDryer.py
+++ TrappedInTheDryer.py
@@ -1,2 +1,18 @@
 #introduction
 print("Welcome to Trapped in the Dryer! This is a text-based game where you have to find your way out of a dryer! Good luck!")
+
+#character setup
+class Character:
+    """Generic character class"""
+    def __init__(self, name, health, attack, defense):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+
+    def stats(self):
+        print(f"""{self.name}'s stats:
+- health: {self.health}
+- attack: {self.attack}
+- defense: {self.defense}
+    """)
