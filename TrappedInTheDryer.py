42ce7620d6e935e8a9e18b4b4c086d2be0161138
Julia <jbian@ucvts.org>
2018-12-28 20:05:34 +0000

    revised introduction

--- TrappedInTheDryer.py
+++ TrappedInTheDryer.py
@@ -1,5 +1,17 @@
 #introduction
-print("Welcome to Trapped in the Dryer! This is a text-based game where you have to find your way out of a dryer! Good luck!")
+print("Welcome to Trapped in the Dryer! This is a text-based game where you have to find your way out of a dryer! Good luck!\n")
+
+start = input("To begin playing, type 'play' and hit enter. Type 'help' to learn how to play. > ").lower()
+while True:
+    if start == "play":
+        begin_game()
+        break
+    elif start == "help":
+        help()
+        break
+    else:
+        while start not in ("play", "help"):
+            start = input("That is not a valid option. Please type 'play' or 'help'. > ")
 
 #character setup
 class Character:
