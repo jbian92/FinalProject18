ca7a5b0a691375b3f576575d074074b45a66add5
Julia <jbian@ucvts.org>
2019-01-03 15:33:58 +0000

    revised beginning of game

--- TrappedInTheDryer.py
+++ TrappedInTheDryer.py
@@ -1,12 +1,38 @@
+#This is for the player to press enter to continue with the descriptions in the game. This will break up the description so it would be easier to read.
+def x():
+    x = input("")
+    return x
+
+#Before beginning the game, the player can choose to either begin or learn how to play.
+def start():
+    start = input("To begin playing, type 'play' and press enter. Type 'help' to learn how to play. > ").lower()
+    while True:
+        if start == "play":
+            begin_game()
+            break
+        elif start == "help":
+            help_info()
+            break
+        else:
+            while start not in ("play", "help"):
+                start = input("That is not a valid option. Please type 'play' or 'help'. > ")
+
 #This tells the player how to play the game.
 def help_info():
     print("\n- - - - - - - - - - - - - - - - - - - - - - - - -\n")
-    print("You will be exploring places and interacting with items in the game.\n")
-    print("You will be asked a question as to what you would like to do. You will be given the choices within the question.\n")
+    print("You will be exploring places and interacting with items in the game. After every line of text that does not require a command, press enter to continue.")
+    x()
+    print("You will be asked a question as to what you would like to do. You will be given the choices within the question.")
+    x()
     print(f"""For example:
 Do you want to 'go through the door' or 'walk up the stairs'?
 You can either type in 'go through the door' or 'walk up the stairs'. The commands you can use will be in quotes.""")
-    print("\n- - - - - - - - - - - - - - - - - - - - - - - - -\n")
+    x()
+    print("If you are not given a command to use, you are most likely being given a description. You will need to press enter to continue.\n")
+    print("~ Press enter to continue. ~")
+    x()
+    print("- - - - - - - - - - - - - - - - - - - - - - - - -\n")
+    start()
 
 #character setup
 class Character:
@@ -25,19 +51,19 @@ class Character:
     """)
 
 def begin_game():
-    print("hello")
+
+    #background information
+    print("\n- - - - - - - - - - - - - - - - - - - - - - - - -\n")
+    print("It's laundry day! Yay!... yeah no. You are at the laundromat with your bundle of clothes. Thankfully, you only have to wait for your clothes to finish drying in the dryer before you can finally go home.")
+    x()
+    print(".")
+    x()
+    print(".")
+    x()
+    print(".")
+    x()
+    print("And the dryer finally stopped! You begin taking out your clothes, excited to go back home and binge watch Netflix on the couch.")
 
 #introduction to the game
 print("Welcome to Trapped in the Dryer! This is a text-based game where you have to find your way out of a dryer! Good luck!\n")
-
-start = input("To begin playing, type 'play' and hit enter. Type 'help' to learn how to play. > ").lower()
-while True:
-    if start == "play":
-        begin_game()
-        break
-    elif start == "help":
-        help_info()
-        break
-    else:
-        while start not in ("play", "help"):
-            start = input("That is not a valid option. Please type 'play' or 'help'. > ")
+start()
